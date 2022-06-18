7715c76fe6d156e039792cd79f60b05469a21f21
Anton Zaharia <zaharia_anton@yahoo.com>
2022-06-18 17:08:26 +0100

    fix iframe

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ iframe.setAttribute('src', videoLink)
 iframe.setAttribute('width', '360')
 iframe.setAttribute('height', '115')
 iframe.setAttribute('frameborder', '0')
+iframe.setAttribute('title', 'YouTube video player')
+iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture')
+iframe.setAttribute('allowfullscreen', 'allowfullscreen')
 
 const box = document.createElement('div')
 box.style.backgroundColor = 'red'
@@ -18,6 +21,9 @@ box.style.left = Xposition === 'left' ? '40px' : 'unset'
 box.style.right = Xposition === 'right' ? '40px' : 'unset'
 box.style.top = Yposition === 'top' ? '40px' : 'unset'
 box.style.bottom = Yposition === 'bottom' ? '40px' : 'unset'
+box.style.display = 'flex'
+box.style.justifyContent = 'center'
+box.style.padding = '20px'
 
 box.appendChild(iframe)
 
