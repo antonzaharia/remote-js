f57ac5f3dee93d224f201b865662b1d7cb699882
Anton Zaharia <zaharia_anton@yahoo.com>
2022-06-18 16:58:31 +0100

    fixes

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const name = 'Anton'
-const Yposition = 'left'
-const Xposition = 'bottom'
+const Xposition = 'right'
+const Yposition = 'bottom'
 const videoLink = 'https://www.youtube.com/watch?v=EywWngU5xDk'
 
 const iframe = document.createElement('div')
 iframe.setAttribute('src', videoLink)
-iframe.setAttribute('width', '560')
-iframe.setAttribute('height', '315')
+iframe.setAttribute('width', '360')
+iframe.setAttribute('height', '115')
 iframe.setAttribute('frameborder', '0')
 
 const box = document.createElement('div')
-box.style.backgroundColor = 'dark-gray'
-box.style.width = '500px'
-box.style.height = '500px'
+box.style.backgroundColor = 'red'
+box.style.width = '400px'
+box.style.height = '255px'
 box.style.position = 'absolute'
 box.style.left = Xposition === 'left' ? '40px' : 'unset'
 box.style.right = Xposition === 'right' ? '40px' : 'unset'
-box.style.top = Xposition === 'top' ? '40px' : 'unset'
-box.style.bottom = Xposition === 'bottom' ? '40px' : 'unset'
+box.style.top = Yposition === 'top' ? '40px' : 'unset'
+box.style.bottom = Yposition === 'bottom' ? '40px' : 'unset'
 
 box.appendChild(iframe)
 
