e6da5e0016869c38126fbce5e3d5a320c21a6e51
Anton Zaharia <zaharia_anton@yahoo.com>
2022-06-18 16:46:48 +0100

    inject and append

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('test')
+const name = 'Anton'
+const Yposition = 'left'
+const Xposition = 'bottom'
+const videoLink = 'https://www.youtube.com/watch?v=EywWngU5xDk'
+
+const iframe = document.createElement('div')
+iframe.textContent = 'Test'
+
+document.querySelector('body').appendChild(iframe)
+
+{
+  /* <iframe width="560" height="315" src="https://www.youtube.com/embed/EywWngU5xDk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */
+}
