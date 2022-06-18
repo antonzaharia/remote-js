08ed67908fe56ab2000108b70db093e968d2a9f9
Anton Zaharia <zaharia_anton@yahoo.com>
2022-06-18 16:54:45 +0100

    style tweaks

--- index.js
+++ index.js
@@ -4,10 +4,21 @@ const Xposition = 'bottom'
 const videoLink = 'https://www.youtube.com/watch?v=EywWngU5xDk'
 
 const iframe = document.createElement('div')
-iframe.textContent = 'Test'
+iframe.setAttribute('src', videoLink)
+iframe.setAttribute('width', '560')
+iframe.setAttribute('height', '315')
+iframe.setAttribute('frameborder', '0')
 
-document.querySelector('body').appendChild(iframe)
+const box = document.createElement('div')
+box.style.backgroundColor = 'dark-gray'
+box.style.width = '500px'
+box.style.height = '500px'
+box.style.position = 'absolute'
+box.style.left = Xposition === 'left' ? '40px' : 'unset'
+box.style.right = Xposition === 'right' ? '40px' : 'unset'
+box.style.top = Xposition === 'top' ? '40px' : 'unset'
+box.style.bottom = Xposition === 'bottom' ? '40px' : 'unset'
 
-{
-  /* <iframe width="560" height="315" src="https://www.youtube.com/embed/EywWngU5xDk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */
-}
+box.appendChild(iframe)
+
+document.querySelector('body').appendChild(box)
